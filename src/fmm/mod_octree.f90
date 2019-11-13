c9b57c65e6ee059c74331d28c22c62e3cdcadfbb
Matteo Tugnoli <tugnoli.teo@gmail.com>
2019-11-13 17:39:40 +0100

    Fixed a bug in articles redistribution

--- src/fmm/mod_octree.f90
+++ src/fmm/mod_octree.f90
@@ -227,6 +227,10 @@ interface push_ptr
   module procedure push_pan_ptr_scalar,  push_pan_ptr_vec
 end interface
 
+interface pull_ptr
+  module procedure pull_part_ptr_scalar
+end interface
+
 !----------------------------------------------------------------------
 character(len=*), parameter :: this_mod_name='mod_octree'
 character(len=max_char_len) :: msg
@@ -839,26 +843,31 @@ subroutine sort_particles(wparts, n_prt, elem, octree)
     do k=1,octree%ncl(3,ll); do j=1,octree%ncl(2,ll); do i = 1,octree%ncl(1,ll)
       if(.not. octree%layers(ll)%lcells(i,j,k)%near_wall) then
         nprt = octree%layers(ll)%lcells(i,j,k)%npart
-        octree%layers(ll)%lcells(i,j,k)%ave_vortmag =  &
-          octree%layers(ll)%lcells(i,j,k)%ave_vortmag/real(nprt,wp)
-
-        do ip=1,nprt
+        if (nprt .gt. 0) then
+          octree%layers(ll)%lcells(i,j,k)%ave_vortmag =  &
+             octree%layers(ll)%lcells(i,j,k)%ave_vortmag/real(nprt,wp)
+        endif
+        
+        ip = 1 
+        !do ip=1,nprt
+        do while (ip.lt.nprt+1)
           if(octree%layers(ll)%lcells(i,j,k)%cell_parts(ip)%p%mag .lt. &
              1.0_wp/sim_param%part_redist_ratio *&
              octree%layers(ll)%lcells(i,j,k)%ave_vortmag) then
             !the particle is significantly smaller than the average in the cell
+            !redistribute it
+            redistr = (octree%layers(ll)%lcells(i,j,k)%cell_parts(ip)%p%mag*&
+                       octree%layers(ll)%lcells(i,j,k)%cell_parts(ip)%p%dir)&
+                       / real(nprt,wp)
             !free
             octree%layers(ll)%lcells(i,j,k)%cell_parts(ip)%p%free = .true.
+            call pull_ptr(octree%layers(ll)%lcells(i,j,k)%cell_parts, ip)
             octree%layers(ll)%lcells(i,j,k)%npart = &
                                       octree%layers(ll)%lcells(i,j,k)%npart - 1
+            nprt = nprt -1
             !lower also the global counter
             n_prt = n_prt -1
 
-            !redistribute it
-            nprt2 = octree%layers(ll)%lcells(i,j,k)%npart
-            redistr = (octree%layers(ll)%lcells(i,j,k)%cell_parts(ip)%p%mag*&
-                       octree%layers(ll)%lcells(i,j,k)%cell_parts(ip)%p%dir)&
-                       / real(nprt2,wp)
             do iq=1,nprt
                 if(.not. octree%layers(ll)%lcells(i,j,k)%cell_parts(iq)%p%free) then
                   vort =  octree%layers(ll)%lcells(i,j,k)%cell_parts(iq)%p%mag*&
@@ -873,6 +882,8 @@ subroutine sort_particles(wparts, n_prt, elem, octree)
                   endif
                 endif !not free particle
             enddo !iq, rest of the particles
+          else
+            ip = ip + 1
           endif !too small particle
         enddo !ip, particles in cell
       endif !near wall
@@ -1011,8 +1022,6 @@ subroutine calculate_multipole(part,octree)
   t0 = dust_time()
 !$omp parallel do private(lv) schedule(guided)
   do lv = 1, octree%nleaves
-  !DEBUG
-  !write(*,*) 'Leaf nr, nr_part, size cell_parts',lv,octree%leaves(lv)%p%npart,size(octree%leaves(lv)%p%cell_parts)
     call octree%leaves(lv)%p%mp%leaf_M(&
                octree%leaves(lv)%p%cen, &
                octree%leaves(lv)%p%cell_parts, &
@@ -1732,6 +1741,25 @@ subroutine push_part_ptr_vec(pointers, elements, last_elem)
   endif
 
 end subroutine push_part_ptr_vec
+
+!----------------------------------------------------------------------
+
+subroutine pull_part_ptr_scalar(pointers, idx)
+ type(t_vortpart_p), allocatable, target, intent(inout) :: pointers(:)
+ integer, intent(in) :: idx
+
+ integer :: i
+
+ !simply shift all the arrray to remove the pointer in position idx
+ !the number of elements should be decreased outside here
+
+ !pointers(idx:size(pointers)-1) = pointers(idx+1:size(pointers))
+ !nullify(pointers(size(pointers))%p)
+  do i=idx,size(pointers)-1
+    pointers(i)%p => pointers(i+1)%p
+  enddo
+
+end subroutine pull_part_ptr_scalar
 
 !----------------------------------------------------------------------
 
