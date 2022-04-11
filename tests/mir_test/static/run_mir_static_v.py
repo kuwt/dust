20d1619c58d19aa93f18cb1c84dfb04404d29122
alberto_27 <alberto.savino@polimi.it>
2022-04-11 18:11:30 +0200

    run_mir_static_v.py file

--- tests/mir_test/static/run_mir_static_v.py
+++ tests/mir_test/static/run_mir_static_v.py
@@ -8,61 +8,45 @@ import math as mth
 import h5py
 
 pars = argparse.ArgumentParser()
-pars.add_argument("exe_path",help='path to the executables')
 pars.add_argument('-t','--tolerance', nargs='?', help='tolerance for the test',
                   required=False, default=1e-10, type=float)
 pars.add_argument('-r','--release', nargs='?', help='release to check with',
                   required=False, default='0.6.0')
-
 args = pars.parse_args()
-exe_path = args.exe_path
-exe_path = os.path.abspath(exe_path)
-pre    = exe_path+'/dust_pre'
-solver = exe_path+'/dust'
-post   = exe_path+'/dust_post'
+
+pre    = 'dust_pre'
+solver = 'dust'
+post   = 'dust_post'
 tol = args.tolerance
-
-vers_dic ={'0.5.11':'0-5-11', '0.5.12':'0-5-12', '0.5.13':'0-5-13',
-           '0.5.14':'0-5-14', '0.6.0':'0-6-0'}
-try:
-  vers = '_'+vers_dic[args.release]
-except:
-  print('No results available for version ',args.release)
-  print('Available versions: ')
-  for key in vers_dic.keys():
-    print(key)
-  exit()
-
 
 pwd = os.getcwd()
 sets_descr = ['Basic wing with mirror']
 
 #run the first set of simulations, set a
-os.chdir("./reg_test_a")
+os.chdir("./tests/mir_test/static/v")
 pre_ret = sbprc.call([pre])
-runs = ['dust_static_base.in','dust_dynamic_base.in']
-descr = ['static, basic configuration','dynamic, basic configuration']
-runs_names = ['static_base', 'dynamic_base']
+runs = ['dust.in']
+descr = ['mirror static vortex lattice']
+runs_names = ['mirror static vortex lattice']
 sol_dsets = ['/ParticleWake/WakePoints', '/ParticleWake/WakeVort', 
       '/Components/Comp001/Solution/Vort', '/Components/Comp001/Solution/Pres',
-      '/Components/Comp002/Solution/Vort', '/Components/Comp002/Solution/Pres',
-      '/Components/Comp003/Solution/Vort', '/Components/Comp003/Solution/Pres']
+      '/Components/Comp002/Solution/Vort', '/Components/Comp002/Solution/Pres']
+
 sol_descr = ['Particles position   ', 'Particles Intensity  ', 
              'Component 1 Intensity', 'Component 1 Pressure ',
-             'Component 2 Intensity', 'Component 2 Pressure ',
-             'Component 3 Intensity', 'Component 3 Pressure ']
+             'Component 2 Intensity', 'Component 2 Pressure ']
+
 errors = np.zeros([len(runs),len(sol_dsets)])
 for i, run in enumerate(runs):
   solv_ret = sbprc.call([solver,run])
 
-  ref_name = 'ref_'+runs_names[i]
-  check_name = 'test_'+runs_names[i]
+  ref_name = 'ref'
+  check_name = 'test'
   suffix = '.h5'
   folder = './Output/'
   res = '_res_0011'
 
-  ref_filename = folder+ref_name+vers+res+suffix
-  #ref_filename = folder+ref_name+res+suffix
+  ref_filename = folder+ref_name+res+suffix
   check_filename = folder+check_name+res+suffix
   ref_file = h5py.File(ref_filename, 'r')
   check_file = h5py.File(check_filename, 'r')
