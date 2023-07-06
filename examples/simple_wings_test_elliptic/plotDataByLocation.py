c7e61dd008bf72766e9e4310ed6e98f65a2502e5
kuwingto <kuwingto@gmail.com>
2023-07-06 16:49:21 +0200

    update case

---
+++ examples/simple_wings_test_elliptic/plotDataByLocation.py
@@ -0,0 +1,53 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+############ input ##################
+pointprobes = 24
+timestep = 150
+
+
+############# utility ###############
+def readlineIntoFloats(line):
+    content = line.split(" ")
+    content_temp = []
+    for elem in content:
+        if elem != '' and elem != '\n' :
+            content_temp.append(float(elem))
+    content = content_temp
+    return content
+##############################
+#print(os.listdir())
+with open('Postprocessing/post_prb01.dat','r') as file:
+    listOfLines = file.readlines()
+    
+    ########## read the position information  #########
+    line = listOfLines[2]
+    content = readlineIntoFloats(line)
+    y_position = content
+    
+    ########## read the information at the selected time step #########
+    line = listOfLines[timestep]
+    content = readlineIntoFloats(line)
+    
+    # obtain the time 
+    time = content.pop(0)
+    ux_table = []
+    uy_table = []
+    uz_table = []
+    while len(content) != 0:
+        ux = content.pop(0)
+        uy = content.pop(0)
+        uz = content.pop(0)
+        ux_table.append(ux)
+        uy_table.append(uy)
+        uz_table.append(uz)
+    
+    plt.plot(y_position,ux_table)
+    plt.plot(y_position,uy_table)
+    plt.plot(y_position,uz_table)
+    plt.legend(["ux","uy", "uz"])
+    plt.show()
+    
+    plt.plot(y_position,uz_table)
+    plt.legend(["uz"])
+    plt.show()
