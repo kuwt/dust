01467bafe7b5d23523cbaafbe0428b397b08f3c8
kuwingto <kuwingto@gmail.com>
2023-08-09 14:51:18 +0200

    update

--- examples/goland_wing2/main_mbdyn.py
+++ examples/goland_wing2/main_mbdyn.py
@@ -70,7 +70,7 @@ nnodes = np.size(rr,0)
 
 ############## setup mbcNodal, the socket communication to mbdyn ##############3
 @dataclass
-class SocketData:
+class MbdynSocketData:
     path: string = "",
     host: string = ""
     port: int = 0
@@ -83,7 +83,7 @@ class SocketData:
     rot: int = 0x100 #!!!!!!!!!!!!!!!!
     accels:int = 1
 
-socketData = SocketData(path = socket_path, nnodes = nnodes)
+socketData = MbdynSocketData(path = socket_path, nnodes = nnodes)
 nodal = mbcNodal(socketData.path, \
                 socketData.host, \
                 socketData.port, \
@@ -93,7 +93,7 @@ nodal = mbcNodal(socketData.path, \
                 socketData.refnode, \
                 socketData.nnodes, \
                 socketData.labels, \
-                0x100, \
+                socketData.rot, \
                 socketData.accels )
 
 nodal.negotiate()
@@ -105,84 +105,62 @@ fid = open('./../nnodes.dat', "w")
 fid.write('%d' % ( socketData.nnodes ) )
 fid.close()
 
-############ setup data dictionary, not sure its exact purpose #################
-nodal_data = { "Position":{ \
-                    "type":"vector", "io":"write", "data":[] }, \
-                  "Velocity":{ \
-                    "type":"vector", "io":"write", "data":[] }, \
-                  "Rotation":{ \
-                    "type":"vector", "io":"write", "data":[] }, \
-                  "AngularVelocity":{ \
-                    "type":"vector", "io":"write", "data":[] }, \
-                  "Force":{ \
-                    "type":"vector", "io":"read" , "data":[] }, \
-                  "Moment":{ \
-                    "type":"vector", "io":"read" , "data":[] } \
-                }
+############ CouplingData structure for temporary data storge #################
+class CouplingData :
+    def __init__(self):
+      self.namelist = [] 
+      self.typelist = []  # currently only vector supported
+      self.iolist = []
+      self.dataDictionary = {}
+
+      self.addField('Position','vector','write')
+      self.addField('Velocity','vector','write')
+      self.addField('Rotation','vector','write')
+      self.addField('AngularVelocity','vector','write')
+      self.addField('Force','vector','read')
+      self.addField('Moment','vector','read')
+
+    def addField(self, name, data_type, io_type):
+      self.namelist.append(name)
+      self.typelist.append(data_type)
+      self.iolist.append(io_type)
+      
+    def setData(self, name, data):
+      self.dataDictionary[name] = data
+
+    def getData(self, name):
+      return self.dataDictionary.get(name)
+
+
+
+couplingData = CouplingData()
 
 n = socketData.nnodes; nd = 3
-nodal_data["Position"       ]["data"] = np.reshape( nodal.n_x    , (n, nd) )
-nodal_data["Velocity"       ]["data"] = np.reshape( nodal.n_xp   , (n, nd) )
-nodal_data["Rotation"       ]["data"] = np.reshape( nodal.n_theta, (n, nd) )
-nodal_data["AngularVelocity"]["data"] = np.reshape( nodal.n_omega, (n, nd) )
-nodal_data["Force" ]["data"] = np.zeros((n, nd))
-nodal_data["Moment"]["data"] = np.zeros((n, nd))
-
-
-############  setup precice data ???? #########################33
-partic = { \
-          'name':'MBDyn', \
-          'mesh':{ \
-              'name':'MBDynNodes', 'id':[], 'node_ids':[], \
-              'nodes':[], 'nnodes':[], 'dim':[] }, \
-          'fields':{}  \
-          }
-field_dict = { 'name':'field_name', 'id':[], 'data':[],
-            'type':'scalar/vector', 'io':'read/write' }
-
-fieldlist = [ 'Position', 'Velocity', 'Rotation', \
-              'AngularVelocity', 'Force', 'Moment' ]
-typelist  = [ 'vector', 'vector', 'vector', \
-              'vector', 'vector', 'vector'  ]
-iolist    = [ 'write', 'write', 'write', 'write', 'read', \
-              'read' ]
+couplingData.setData("Position",np.reshape( nodal.n_x , (n, nd) ))
+couplingData.setData("Velocity",np.reshape( nodal.n_xp , (n, nd) ))
+couplingData.setData("Rotation",np.reshape( nodal.n_theta , (n, nd) ))
+couplingData.setData("AngularVelocity",np.reshape( nodal.n_omega , (n, nd) ))
+couplingData.setData("Force",np.zeros((n, nd)))
+couplingData.setData("Moment",np.zeros((n, nd)))
 
 ############## setup precice interface #################
+participant_name = 'MBDyn'
 precice_config_file_name = './../precice-config.xml'
 comm_rank = 0
 comm_size = 1   
+precice_interface = precice.Interface( participant_name, precice_config_file_name, comm_rank, comm_size )
 
-precice_interface = precice.Interface( partic['name'], \
-                                    precice_config_file_name, \
-                                    comm_rank, comm_size )
+mesh_id = precice_interface.get_mesh_id( 'MBDynNodes')
+vertex_ids = precice_interface.set_mesh_vertices( mesh_id, rr )
 
-
-############## fill in information to partic #################
-partic['mesh']['id'] = \
-         precice_interface.get_mesh_id( partic['mesh']['name'] )
-
-for i in np.arange( len(fieldlist) ):
-  fieldn = fieldlist[i]
-  if ( precice_interface.has_data( fieldn,partic['mesh']['id'] ) ):
-    field = field_dict.copy()
-    field['name'] = fieldn
-    field['id']   = precice_interface.get_data_id( field['name'], \
-                                        partic['mesh']['id'] )
-    field['type'] = nodal_data[fieldn]['type']
-    field['io']   = nodal_data[fieldn]['io']
-    partic['fields'][field['name']] = field
-
-partic['mesh']['nodes'] = rr 
-partic['mesh']['nnodes'] = np.size( nodal_data['Position']['data'], 0 )
-partic['mesh']['dim']    = np.size( nodal_data['Position']['data'], 1 )
-partic['mesh']['node_id'] = precice_interface.set_mesh_vertices( \
-                partic['mesh']['id'], partic['mesh']['nodes'] )
 
 ############# initialize precice #######################
 dt_precice = precice_interface.initialize()
 precice_interface.initialize_data()
 
-########### run precice #########################
+################################################
+###         Main loop                     ########
+###################################################
 cowic = precice.action_write_iteration_checkpoint()
 coric = precice.action_read_iteration_checkpoint()
 force = np.zeros((n, nd))
@@ -197,16 +175,16 @@ while ( is_ongoing ):
   print('\033[0m   Iteration ▶ ', niter)
   
   if ( precice_interface.is_action_required( cowic ) ):
-    pos_t = nodal_data['Position']['data'][:,:] 
-    vel_t = nodal_data['Velocity']['data'][:,:] 
-    rot_t = nodal_data['Rotation']['data'][:,:] 
-    ome_t = nodal_data['AngularVelocity']['data'][:,:]
+    pos_t = couplingData.getData("Position")
+    vel_t = couplingData.getData("Velocity")
+    rot_t = couplingData.getData("Rotation")
+    ome_t = couplingData.getData("AngularVelocity")
     precice_interface.mark_action_fulfilled( cowic )
 
   #> Set MBDyn nodal values of forces and moments
   for i in np.arange(n):
-    nodal.n_f[i*nd:(i+1)*nd] = nodal_data['Force' ]['data'][i,:] 
-    nodal.n_m[i*nd:(i+1)*nd] = nodal_data['Moment']['data'][i,:] 
+    nodal.n_f[i*nd:(i+1)*nd] = couplingData.getData("Force")[i,:] 
+    nodal.n_m[i*nd:(i+1)*nd] = couplingData.getData("Moment")[i,:] 
   
   dt = min( dt_input, dt_precice )
 
@@ -219,49 +197,40 @@ while ( is_ongoing ):
     print('**** break, after nodal.recv() ****'); break
 
   #> Read position and velocity from MBDyn
-  nodal_data['Position'       ]['data'] = np.reshape( nodal.n_x    , (n, nd)) 
-  nodal_data['Velocity'       ]['data'] = np.reshape( nodal.n_xp   , (n, nd))
-  nodal_data['Rotation'       ]['data'] = np.reshape( nodal.n_theta, (n, nd))
-  nodal_data['AngularVelocity']['data'] = np.reshape( nodal.n_omega, (n, nd))
+  couplingData.setData("Position",np.reshape( nodal.n_x , (n, nd) ))
+  couplingData.setData("Velocity",np.reshape( nodal.n_xp , (n, nd) ))
+  couplingData.setData("Rotation",np.reshape( nodal.n_theta , (n, nd) ))
+  couplingData.setData("AngularVelocity",np.reshape( nodal.n_omega , (n, nd) ))
 
   #> Write to AeroSolver
-  for fie in partic['fields']:
-    if ( partic['fields'][fie]['io'] == 'write' ):
-      if ( partic['fields'][fie]['type'] == 'scalar' ):
-        precice_interface.write_block_scalar_data( \
-                                partic['fields'][fie]['id'], \
-                                partic['mesh']['node_id'],   \
-                                nodal_data[fie]['data'] )
-      if ( partic['fields'][fie]['type'] == 'vector' ):
-        precice_interface.write_block_vector_data( \
-                                partic['fields'][fie]['id'], \
-                                partic['mesh']['node_id'],   \
-                                nodal_data[fie]['data'] )
+  data_id = precice_interface.get_data_id( 'Position', mesh_id )
+  precice_interface.write_block_vector_data( data_id, vertex_ids,couplingData.getData("Position"))
+  data_id = precice_interface.get_data_id( 'Velocity', mesh_id )
+  precice_interface.write_block_vector_data( data_id, vertex_ids,couplingData.getData("Velocity"))
+  data_id = precice_interface.get_data_id( 'Rotation', mesh_id )
+  precice_interface.write_block_vector_data( data_id, vertex_ids,couplingData.getData("Rotation"))
+  data_id = precice_interface.get_data_id( 'AngularVelocity', mesh_id )
+  precice_interface.write_block_vector_data( data_id, vertex_ids,couplingData.getData("AngularVelocity"))
 
   #> advance steo
   is_ongoing = precice_interface.is_coupling_ongoing()
   dt_precice = precice_interface.advance(dt)
 
   #> Receive data from AeroSolver and set nodal.n_f field
-  for fie in partic['fields']:
-    if ( partic['fields'][fie]['io'] == 'read' ):
-      if ( partic['fields'][fie]['type'] == 'scalar' ):
-        nodal_data[fie]['data'] = \
-          precice_interface.read_block_scalar_data( \
-                                partic['fields'][fie]['id'], \
-                                partic['mesh']['node_id']    )
-      if ( partic['fields'][fie]['type'] == 'vector' ):
-        nodal_data[fie]['data'] = \
-          precice_interface.read_block_vector_data( \
-                                partic['fields'][fie]['id'], \
-                                partic['mesh']['node_id']    )
+  data_id = precice_interface.get_data_id( 'Force', mesh_id )
+  data = precice_interface.read_block_vector_data( data_id, vertex_ids)
+  couplingData.setData("Force",data)
+  data_id = precice_interface.get_data_id( 'Moment', mesh_id )
+  data = precice_interface.read_block_vector_data( data_id, vertex_ids)
+  couplingData.setData("Moment",data)
 
   #> Check convergence: iterate or finalize the timestep
   if ( precice_interface.is_action_required( coric ) ): # dt not converged
-    nodal_data['Position'       ]['data'] = pos_t
-    nodal_data['Velocity'       ]['data'] = vel_t
-    nodal_data['Rotation'       ]['data'] = rot_t
-    nodal_data['AngularVelocity']['data'] = ome_t
+    couplingData.setData("Position",pos_t)
+    couplingData.setData("Velocity",vel_t)
+    couplingData.setData("Rotation",rot_t)
+    couplingData.setData("AngularVelocity",ome_t)
+
     precice_interface.mark_action_fulfilled( coric )
   else: # dt converged
     if (nodal.send(True) ):
